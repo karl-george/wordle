2a391f8cdc61dc413f9c7f89282803b27bd59a34
Karl George <karlgeorge87@gmail.com>
2024-09-23 12:18:33 +0200

    Add SVG to page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Icon from '@/assets/images/wordle-icon.svg';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello.</Text>
+    <View style={styles.container}>
+      <Icon width={100} height={100} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
