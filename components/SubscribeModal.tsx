b83973d2d0835bb5c398462614e2b6b2b0cd5591
Karl George <karlgeorge87@gmail.com>
2024-09-23 13:39:57 +0200

    Add marked list and disclaimer

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -10,8 +10,18 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Link } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { Colors } from '@/constants/Colors';
+import MarkedList from '@jsamr/react-native-li';
+import disc from '@jsamr/counter-style/presets/disc';
 
 export type Ref = BottomSheetModal;
+
+const BENEFITS = [
+  'Enjoy full access to Wordle, Spelling Bee, The Crossword and more.',
+  'Play new puzzles every day for concentration or relaxation.',
+  'Strengthen your strategy with WordleBot.',
+  'Unlock over 10,000 puzzles in our Wordle, Spelling Bee and crossword archives.',
+  'Track your stats and streaks on any device.',
+];
 
 const SubscribeModal = forwardRef<Ref>((props, ref) => {
   const snapPoints = useMemo(() => ['90%'], []);
@@ -59,7 +69,41 @@ const SubscribeModal = forwardRef<Ref>((props, ref) => {
             source={require('../assets/images/games.png')}
             style={styles.image}
           />
+
+          <View style={{ marginVertical: 20 }}>
+            <MarkedList
+              counterRenderer={disc}
+              lineStyle={{ paddingHorizontal: 40, gap: 10, marginVertical: 10 }}
+            >
+              {BENEFITS.map((value, index) => (
+                <Text key={index} style={styles.listText}>
+                  {value}
+                </Text>
+              ))}
+            </MarkedList>
+          </View>
+          <Text style={styles.disclaimer}>
+            If you subscribe to The New York Times via this app, payment for
+            your subscription will be automatically charged to your Apple ID
+            account upon your confirmation of purchase with Apple. Your Apple ID
+            account will be automatically charged for renewal at the applicable
+            rate shown to you at the time of subscription every calendar month
+            (for monthly subscriptions) or every year (for annual subscriptions)
+            within 24-hours prior to the start of your next billing period. For
+            special introductory offers, you will be automatically charged the
+            applicable introductory rate shown to you at the time of
+            subscription for the stated introductory period and the standard
+            rate rate shown to you at the time of subscription thereafter. You
+            will be charged in advance. Subscriptions continue automatically
+            until you cancel. Cancellation takes effect at the end of your
+            current billing period. You can manage and cancel subscriptions in
+            your account settings on the App Store. To cancel, please turn off
+            auto-renew at lead; 24-hours before the end of your current billing
+            period from your iTunes account settings.
+          </Text>
         </BottomSheetScrollView>
+
+        <View></View>
       </View>
     </BottomSheetModal>
   );
@@ -94,5 +138,18 @@ const styles = StyleSheet.create({
     width: '90%',
     height: 40,
     alignSelf: 'center',
+  },
+  listText: {
+    fontSize: 14,
+    flexShrink: 1,
+    color: '#4f4f4f',
+  },
+  disclaimer: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#484848',
+    marginHorizontal: 30,
+    lineHeight: 18,
+    marginBottom: 20,
   },
 });
