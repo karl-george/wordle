69eb127c9ef00acefe54455420d2fa4493b2cb9c
Karl George <karlgeorge87@gmail.com>
2024-09-23 13:11:37 +0200

    Add bottom sheet provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,20 @@
-import * as SplashScreen from 'expo-splash-screen';
-import { Stack } from 'expo-router';
 import {
-  useFonts,
-  FrankRuhlLibre_800ExtraBold,
   FrankRuhlLibre_500Medium,
+  FrankRuhlLibre_800ExtraBold,
   FrankRuhlLibre_900Black,
+  useFonts,
 } from '@expo-google-fonts/frank-ruhl-libre';
-import { useEffect } from 'react';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 // Load the fonts first before hiding the splash screen
 SplashScreen.preventAutoHideAsync;
@@ -37,9 +39,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name='index' options={{ headerShown: false }} />
-      </Stack>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <Stack>
+            <Stack.Screen name='index' options={{ headerShown: false }} />
+          </Stack>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </ThemeProvider>
   );
 }
