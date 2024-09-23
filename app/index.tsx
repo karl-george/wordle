40eac699a0c00ddfe173180c339385faf6503b3e
Karl George <karlgeorge87@gmail.com>
2024-09-23 12:43:14 +0200

    Style buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Icon from '@/assets/images/wordle-icon.svg';
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      {/* Header */}
       <View style={styles.header}>
         <Icon width={100} height={70} />
         <Text style={styles.title}>Wordle</Text>
         <Text style={styles.text}>Get 6 chances to guess a 5-letter word</Text>
       </View>
-      <View>
-        <Text style={styles.text}>Start a new game</Text>
+      {/* Menu Buttons */}
+      <View style={styles.menu}>
+        <Link
+          href='/game'
+          asChild
+          style={[styles.button, { backgroundColor: '#000' }]}
+        >
+          <TouchableOpacity>
+            <Text style={[styles.btnText, styles.primaryText]}>Play</Text>
+          </TouchableOpacity>
+        </Link>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.btnText}>Log in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.btnText}>Subscribe</Text>
+        </TouchableOpacity>
       </View>
+      {/* Footer */}
       <View style={styles.footer}>
         <Text style={styles.text}>Made by Karl</Text>
       </View>
@@ -32,10 +52,35 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 40,
+    fontFamily: 'FrankRuhlLibre_800ExtraBold',
   },
   text: {
     fontSize: 26,
     textAlign: 'center',
+    fontFamily: 'FrankRuhlLibre_500Medium',
+  },
+  menu: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    borderColor: '#000',
+    borderWidth: 1,
+    width: '60%',
+    maxWidth: 200,
+  },
+  btnText: {
+    padding: 14,
+    fontSize: 16,
+    fontWeight: 'semibold',
+    color: '#333',
+  },
+  primaryText: {
+    color: '#fff',
   },
   footer: {},
 });
