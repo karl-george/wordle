114b49b7012fae28af7256081c785a75d8e0e121
Karl George <karlgeorge87@gmail.com>
2024-09-23 12:54:40 +0200

    Add swapping to darkmode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,11 +7,18 @@ import {
   FrankRuhlLibre_900Black,
 } from '@expo-google-fonts/frank-ruhl-libre';
 import { useEffect } from 'react';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 
 // Load the fonts first before hiding the splash screen
 SplashScreen.preventAutoHideAsync;
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   let [fontsLoaded] = useFonts({
     FrankRuhlLibre_800ExtraBold,
     FrankRuhlLibre_500Medium,
@@ -29,8 +36,10 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false }} />
-    </Stack>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name='index' options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
