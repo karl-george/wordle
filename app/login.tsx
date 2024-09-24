f8fa1bc68872cc922e8e1c0a5f88c4d5091e84fd
Karl George <karlgeorge87@gmail.com>
2024-09-24 12:41:53 +0200

    Add email field

--- app/login.tsx
+++ app/login.tsx
@@ -1,14 +1,82 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { defaultStyles } from '@/constants/Styles';
+import { useOAuth } from '@clerk/clerk-expo';
+import { useRouter } from 'expo-router';
 import React from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+enum Strategy {
+  Google = 'oauth_google',
+  Apple = 'oauth_apple',
+  Facebook = 'oauth_facebook',
+}
 
 const Page = () => {
+  const router = useRouter();
+
+  const { startOAuthFlow: googleAuth } = useOAuth({
+    strategy: Strategy.Google,
+  });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: Strategy.Apple });
+  const { startOAuthFlow: facebookAuth } = useOAuth({
+    strategy: Strategy.Facebook,
+  });
+
+  const onSelectAuth = async (strategy: Strategy) => {};
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Log in or create an account</Text>
+      <Text style={styles.subText}>
+        By continuing, you agree to our Terms of Service and Privacy Policy
+      </Text>
+
+      <Text style={styles.inputLabel}>Email</Text>
+      <TextInput style={styles.input} placeholder='Email' />
+
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingHorizontal: 40,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingTop: 30,
+    paddingBottom: 20,
+    textAlign: 'center',
+  },
+  subText: {
+    fontSize: 15,
+    color: '#4f4f4f',
+    textAlign: 'center',
+    marginBottom: 30,
+  },
+  inputLabel: {
+    paddingBottom: 5,
+  },
+  input: {
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+  },
+});
