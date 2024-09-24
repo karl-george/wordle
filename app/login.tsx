1b42ff7bda7507f1093a05e011a45e131b659471
Karl George <karlgeorge87@gmail.com>
2024-09-24 12:55:43 +0200

    Add oauth buttons

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,7 @@
+import { Colors } from '@/constants/Colors';
 import { defaultStyles } from '@/constants/Styles';
 import { useOAuth } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import React from 'react';
 import {
@@ -37,12 +39,57 @@ const Page = () => {
         By continuing, you agree to our Terms of Service and Privacy Policy
       </Text>
 
+      {/* Email Form */}
       <Text style={styles.inputLabel}>Email</Text>
       <TextInput style={styles.input} placeholder='Email' />
 
       <TouchableOpacity style={defaultStyles.btn}>
         <Text style={defaultStyles.btnText}>Continue</Text>
       </TouchableOpacity>
+
+      {/* Seperator */}
+      <View style={styles.seperatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: '#000',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        ></View>
+        <Text style={styles.seperator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: '#000',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        ></View>
+      </View>
+
+      {/* OAuth */}
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Google)}
+        >
+          <Ionicons name='logo-google' size={24} style={styles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Apple)}
+        >
+          <Ionicons name='logo-apple' size={24} style={styles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Facebook)}
+        >
+          <Ionicons name='logo-facebook' size={24} style={styles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 };
@@ -78,5 +125,34 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     marginBottom: 15,
     paddingHorizontal: 10,
+  },
+  seperatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  seperator: {
+    fontSize: 16,
+    color: Colors.light.gray,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#000',
+    height: 50,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  btnIcon: {
+    paddingRight: 10,
   },
 });
