98780565c392e29afee5dee8df34c37536906a67
Karl George <karlgeorge87@gmail.com>
2024-10-01 13:18:12 +0200

    Setup game states

--- app/game.tsx
+++ app/game.tsx
@@ -1,7 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import React, { useState } from 'react';
+import { Colors } from '@/constants/Colors';
+import { useRouter } from 'expo-router';
+
+const ROWS = 6;
+const COLUMNS = 5;
 
 const Page = () => {
+  // State for board
+  const [rows, setRows] = useState<string[][]>(
+    new Array(ROWS).fill(new Array(COLUMNS).fill(''))
+  );
+  const [currentRow, setCurrentRow] = useState(0);
+  const [currentColumn, setCurrentColumn] = useState(0);
+
+  // State for coloured letters
+  const [greenLetters, setGreenLetters] = useState<string[]>([]);
+  const [yellowLetters, setYellowLetters] = useState<string[]>([]);
+  const [grayLetters, setGrayLetters] = useState<string[]>([]);
+
+  // Theme colour scheme
+  const colorScheme = useColorScheme();
+  const backgroundColor = Colors[colorScheme ?? 'light'].gameBg;
+  const textColor = Colors[colorScheme ?? 'light'].text;
+  const grayColor = Colors[colorScheme ?? 'light'].gray;
+
+  const router = useRouter();
+
+  const addKey = (key: string) => {
+    console.log('addKey', key);
+    // todo
+  };
+
   return (
     <View>
       <Text>Page</Text>
