13cd69c6aa02cf36071d1e7b5496c1636dfa96fa
Karl George <karlgeorge87@gmail.com>
2024-10-01 13:36:19 +0200

    Setup keyboard data

--- components/OnScreenKeyboard.tsx
+++ components/OnScreenKeyboard.tsx
@@ -1,7 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import React from 'react';
 
-const OnScreenKeyboard = () => {
+type Props = {
+  onKeyPressed: (key: string) => void;
+  greenLetters: string[];
+  yellowLetters: string[];
+  grayLetters: string[];
+};
+
+export const ENTER = 'ENTER';
+export const BACKSPACE = 'BACKSPACE';
+
+const keys = [
+  ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+  [ENTER, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', BACKSPACE],
+];
+
+const OnScreenKeyboard = ({
+  onKeyPressed,
+  greenLetters,
+  yellowLetters,
+  grayLetters,
+}: Props) => {
+  const { width } = useWindowDimensions();
+  const keyWidth = width - 60 / keys[0].length;
+  const keyHeight = 60;
+
+  const isSpecialKey = (key: string) => ['ENTER', 'BACKSPACE'].includes(key);
+
+  const isInLetters = (key: string) =>
+    [...greenLetters, ...yellowLetters, ...grayLetters].includes(key);
+
   return (
     <View>
       <Text>OnScreenKeyboard</Text>
