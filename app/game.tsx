c13660f5566caeacea39569b08028a7942a073dd
Karl George <karlgeorge87@gmail.com>
2024-10-01 13:31:32 +0200

    Style the game field

--- app/game.tsx
+++ app/game.tsx
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 import React, { useState } from 'react';
 import { Colors } from '@/constants/Colors';
-import { useRouter } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
+import OnScreenKeyboard from '../components/OnScreenKeyboard';
+import { Ionicons } from '@expo/vector-icons';
 
 const ROWS = 6;
 const COLUMNS = 5;
@@ -33,12 +35,67 @@ const Page = () => {
   };
 
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={[styles.container, { backgroundColor }]}>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <View style={styles.headerIcon}>
+              <Ionicons
+                name='help-circle-outline'
+                size={28}
+                color={textColor}
+              />
+              <Ionicons name='podium-outline' size={28} color={textColor} />
+              <Ionicons name='settings-sharp' size={28} color={textColor} />
+            </View>
+          ),
+        }}
+      />
+      <View style={styles.gameField}>
+        {rows.map((row, rowIndex) => (
+          <View key={`row-${rowIndex}`} style={styles.gameFieldRow}>
+            {row.map((cell, cellIndex) => (
+              <View key={`cell-${rowIndex}-${cellIndex}`} style={styles.cell}>
+                <Text style={styles.cellText}>{cell}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+      <OnScreenKeyboard />
     </View>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 40,
+  },
+  headerIcon: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+  gameField: {
+    alignItems: 'center',
+    gap: 8,
+  },
+  gameFieldRow: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+  cell: {
+    width: 62,
+    height: 62,
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cellText: {
+    fontSize: 30,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+});
